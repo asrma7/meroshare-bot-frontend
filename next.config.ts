e357325b96187bfd7427145a00a327c106603025
Ashutosh Sharma <asrma.sharma@gmail.com>
2025-09-01 11:43:08 -0400

    feat: add Docker configuration and PWA support in Next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  output: 'standalone',
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
